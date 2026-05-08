1183607546a68a748fda269f1587eed76230d8cc
DutchNugget <232434153+DutchNugget@users.noreply.github.com>
2026-05-08 13:14:01 +0200

    added math functions to script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Math functions 
+function addition (a,b) {
+    return a + b;
+}
+
+
+function subtraction (a,b) {
+    return a - b;
+}
+
+
+function multiplication (a,b) {
+    return a * b;
+}
+
+
+function division (a,b) {
+    if (b === 0) {
+        return "Error"
+    }
+
+    return a / b;
+}
+
+
+function percentage (a) {
+    return a / 100
+}
